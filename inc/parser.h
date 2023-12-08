28e5d722063bdbef0a7feb72f34ba17a32feee95
alsaeed3 <ali.eissa.570@gmail.com>
2023-12-08 20:35:54 +0400

    adding src, inc, Makefile

---
+++ inc/parser.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 11:27:45 by alsaeed           #+#    #+#             */
+/*   Updated: 2023/12/06 19:34:17 by alsaeed          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+# include <stdio.h>       // printf
+# include <stdlib.h>      // malloc, free, exit
+# include <unistd.h>      // write, access, fork, execve, getpid
+# include <sys/types.h>   // pid_t
+# include <sys/wait.h>    // wait, waitpid, wait3, wait4
+# include <signal.h>      // signal, sigaction, sigemptyset, sigaddset, kill
+# include <errno.h>       // perror, strerror
+# include <fcntl.h>       // open, close, dup, dup2, pipe
+# include <sys/stat.h>    // stat, lstat, fstat
+# include <dirent.h>      // opendir, readdir, closedir
+# include <unistd.h>      // unlink, getcwd, chdir
+# include <termios.h>     // tcsetattr, tcgetattr
+# include <curses.h>      // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <sys/ioctl.h>   // ioctl
+# include <getopt.h>      // getopt
+# include <limits.h>      // PATH_MAX
+
+
+typedef struct s_
+
+#endif
