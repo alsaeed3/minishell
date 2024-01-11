c46d4c193fd4c7d9dad47bc21668bc1db4ebb54c
AbuEyad <e0e0e2@hotmail.com>
2024-01-11 23:31:11 +0400

    builtins &

---
+++ builtins/env.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: habu-zua <habu-zua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 23:05:49 by habu-zua          #+#    #+#             */
+/*   Updated: 2024/01/11 23:28:52 by habu-zua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/main.h"
+
+void	free_env(char **env)
+{
+	int	i;
+	int	env_len;
+
+	i = 0;
+	env_len = envlen(env);
+	while (i < env_len)
+		free(env[i++]);
+	free(env);
+}
+
+int		envlen(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+		i++;
+	return (++i);
+}
+
+char	**dup_env(char **env)
+{
+	char	**data_env;
+	int		i;
+
+	i = 0;
+	data_env = malloc(sizeof(char *) * envlen(env));
+	if (!data_env)
+		exit(EXIT_FAILURE);
+	while (env[i])
+	{
+		data_env[i] = ft_strdup(env[i]);
+		i++;
+	}
+	data_env[i] = 0;
+	return (data_env);
+}
+
+void	handle_env(char **env, t_data *data)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (env[i])
+	{
+		str = ft_strstr(env[i], "=");
+		if (str)
+		{
+			j = 0;
+			while (env[i][j])
+			{
+				write(1, &env[i][j], 1);
+				j++;
+			}
+			ft_putchar('\n');
+		}
+		i++;
+	}
+	data->status = 0;
+}
---
+++ utils/pipe.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: habu-zua <habu-zua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 23:06:04 by habu-zua          #+#    #+#             */
+/*   Updated: 2024/01/11 23:27:09 by habu-zua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/main.h"
+
+void	handle_parentps(char *input2, t_data *data, int pid, int *fds)
+{
+	int		oldfd;
+	int		status;
+
+	if (waitpid(pid, &status, 0) != pid)
+		exit(EXIT_FAILURE);
+	free(data->user_input);
+	data->user_input = NULL;
+	oldfd = dup(0);
+	dup2(fds[0], 0);
+	close(fds[0]);
+	close(fds[1]);
+	parser_start(input2, data);
+	dup2(oldfd, 0);
+	close(oldfd);
+}
+
+int		handle_pipe(char *input1, char *input2, t_data *data)
+{
+	pid_t	pid;
+	int		fds[2];
+
+	printf("input1: %s\n", input1);
+	printf("input2: %s\n", input2);
+	if (pipe(fds) < 0)
+		exit(EXIT_FAILURE);
+	pid = fork();
+	if (pid == 0) // child
+	{
+		free(input2);
+		dup2(fds[1], 1);
+		close(fds[0]);
+		close(fds[1]);
+		handle_basic(input1, data, 1);
+	}
+	else if (pid < 0)
+		exit(EXIT_FAILURE);
+	else
+	{
+		free(input1);
+		input1 = NULL;
+		handle_parentps(input2, data, pid, fds);
+	}
+	return (1);
+}
