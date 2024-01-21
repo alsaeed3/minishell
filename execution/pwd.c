a007cc515f743f0899167899655164606ef5a04c
AbuEyad <e0e0e2@hotmail.com>
2024-01-21 16:39:10 +0400

    fix env for variable +=

--- execution/pwd.c
+++ execution/pwd.c
@@ -6,13 +6,14 @@
 /*   By: habu-zua <habu-zua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 17:27:07 by habu-zua          #+#    #+#             */
-/*   Updated: 2024/01/20 11:10:21 by habu-zua         ###   ########.fr       */
+/*   Updated: 2024/01/21 14:43:32 by habu-zua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/exec.h"
 void	handle_pwd(t_parse *data)
 {
+	printf("handle_pwd\n");
 	ft_putstr(data->pwd);
 	ft_putstr("\n");
 	// g_status = 0;
