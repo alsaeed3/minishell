201ca6c5edf6cff1dbc14aea2f93ba1631642b83
alsaeed3 <ali.eissa.570@gmail.com>
2024-01-28 19:37:17 +0400

    fix dollar expand in case of redirections

--- parsing/expand_dollar2env.c
+++ parsing/expand_dollar2env.c
@@ -6,7 +6,7 @@
 /*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:17:54 by alsaeed           #+#    #+#             */
-/*   Updated: 2024/01/28 19:01:25 by alsaeed          ###   ########.fr       */
+/*   Updated: 2024/01/28 19:36:30 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,52 +29,62 @@ static int	find_env_size(char *cmd_line, int i)
 	return (size);		
 }
 
-static int expand_dollar_count(char *cmd_line, t_env *env_lst)
+static int expand_dollar_count(char *str, t_env *env_lst)
 {
 	t_bool	solo_quo_trigger;
 	t_bool	duo_quo_trigger;
 	t_bool	dollar_trigger;
+	t_bool	redi_trigger;
 	char	*env;
 	int		len;
 	int		i;
 	int		j;
 	size_t	exp_size;
 	
-	len = ft_strlen(cmd_line);
+	len = ft_strlen(str);
 	solo_quo_trigger = FALSE;
 	duo_quo_trigger = FALSE;
 	dollar_trigger = FALSE;
+	redi_trigger = FALSE;
 	i = -1;
 	j = 0;
 	env = NULL;
 	exp_size = 0;
-	while (++i < len && cmd_line[i])
+	while (++i < len && str[i])
 	{
-		if (cmd_line[i] == '\'' && !solo_quo_trigger && !duo_quo_trigger)
+		if (str[i] == '\'' && !solo_quo_trigger && !duo_quo_trigger)
 		{
 			++exp_size;
 			i++;
 			solo_quo_trigger = TRUE;
 		}
-		if (cmd_line[i] == '"' && !duo_quo_trigger && !solo_quo_trigger)
+		if (str[i] == '"' && !duo_quo_trigger && !solo_quo_trigger)
 		{
 			++exp_size;
 			i++;
 			duo_quo_trigger = TRUE;
 		}
-		if (i < len - 2 && cmd_line[i] == '$' && !(cmd_line[i + 1] >= 65 && cmd_line[i + 1] <= 90) && !(cmd_line[i + 1] >= 97 && cmd_line[i + 1] <= 122) && !solo_quo_trigger)
+		if (i < len - 1 && ((str[i] == '<' && str[i + 1] != '<' && (i == 0 || str[i - 1] != '<')) || (str[i] == '>' && str[i + 1] != '>' && (i == 0 || str[i - 1] != '>'))) && !solo_quo_trigger && !duo_quo_trigger)
+			redi_trigger = TRUE;
+		else if (i < len - 1 && ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>')) && !solo_quo_trigger && !duo_quo_trigger)
+			redi_trigger = TRUE;
+		else if (i < len - 1 && ((str[i] == '<' && str[i + 1] != '<' && (i == 0 || str[i - 1] != '<')) || (str[i] == '>' && str[i + 1] != '>' && (i == 0 || str[i - 1] != '>'))) && (solo_quo_trigger || duo_quo_trigger))
+			redi_trigger = FALSE;
+		else if (i < len - 1 && ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>')) && (solo_quo_trigger || duo_quo_trigger))
+			redi_trigger = FALSE;
+		if (i < len - 2 && str[i] == '$' && !(str[i + 1] >= 65 && str[i + 1] <= 90) && !(str[i + 1] >= 97 && str[i + 1] <= 122) && !solo_quo_trigger)
 			i += 2;
-		else if (cmd_line[i] == '$' && ((cmd_line[i + 1] >= 65 && cmd_line[i + 1] <= 90) || (cmd_line[i + 1] >= 97 && cmd_line[i + 1] <= 122)) && !solo_quo_trigger)
+		else if (str[i] == '$' && ((str[i + 1] >= 65 && str[i + 1] <= 90) || (str[i + 1] >= 97 && str[i + 1] <= 122)) && !solo_quo_trigger && !redi_trigger)
 			dollar_trigger = TRUE;
-		if (dollar_trigger && ((cmd_line[i] >= 65 && cmd_line[i] <= 90) || (cmd_line[i] >= 97 && cmd_line[i] <= 122) || (cmd_line[i] >= 48 && cmd_line[i] <= 57)))
+		if (dollar_trigger && ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122)))
 		{
-				env = ft_calloc(find_env_size(cmd_line, i) + 1, sizeof(char));
+				env = ft_calloc(find_env_size(str, i) + 1, sizeof(char));
 				if (!env)
 					return (0);
 				j = 0;
-				env[j++] = cmd_line[i];
-				while(++i < len && ((cmd_line[i] >= 65 && cmd_line[i] <= 90) || (cmd_line[i] >= 97 && cmd_line[i] <= 122) || (cmd_line[i] >= 48 && cmd_line[i] <= 57)))
-					env[j++] = cmd_line[i];
+				env[j++] = str[i];
+				while(++i < len && ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) || (str[i] >= 48 && str[i] <= 57)))
+					env[j++] = str[i];
 				env[j] = '\0';
 				dollar_trigger = FALSE;
 				if (ft_getenv(env, env_lst))
@@ -85,31 +95,32 @@ static int expand_dollar_count(char *cmd_line, t_env *env_lst)
 					env = NULL;
 				}
 		}
-		if (cmd_line[i] == '\'' && solo_quo_trigger && !duo_quo_trigger)
+		if (str[i] == '\'' && solo_quo_trigger && !duo_quo_trigger)
 		{
 			exp_size++;
 			i++;
 			solo_quo_trigger = FALSE;
 		}
-		else if (cmd_line[i] == '"' && duo_quo_trigger && !solo_quo_trigger)
+		else if (str[i] == '"' && duo_quo_trigger && !solo_quo_trigger)
 		{
 			exp_size++;
 			i++;
 			duo_quo_trigger = FALSE;
 		}
-		if (cmd_line[i] == '$' && ((cmd_line[i + 1] >= 65 && cmd_line[i + 1] <= 90) || (cmd_line[i + 1] >= 97 && cmd_line[i + 1] <= 122)) && !dollar_trigger && !solo_quo_trigger)
+		if (str[i] == '$' && ((str[i + 1] >= 65 && str[i + 1] <= 90) || (str[i + 1] >= 97 && str[i + 1] <= 122)) && !dollar_trigger && !solo_quo_trigger && !redi_trigger)
 			dollar_trigger = TRUE;
-		else if (cmd_line[i] && (!dollar_trigger || cmd_line[i] != '$'))
+		else if (str[i] && (!dollar_trigger || str[i] != '$'))
 			exp_size++;
 	}
 	return (exp_size);
 }
 
-char *expand_dollar_string(char *cmd_line, t_env *env_lst)
+char *expand_dollar_string(char *str, t_env *env_lst)
 {
 	t_bool	solo_quo_trigger;
 	t_bool	duo_quo_trigger;
 	t_bool	dollar_trigger;
+	t_bool	redi_trigger;
 	char	*env;
 	int		len;
 	int		i;
@@ -120,44 +131,53 @@ char *expand_dollar_string(char *cmd_line, t_env *env_lst)
 	char	*env_ret;
 
 	env_ret = NULL;
-	exp_len = expand_dollar_count(cmd_line, env_lst);
+	exp_len = expand_dollar_count(str, env_lst);
 	ret = ft_calloc(exp_len + 1, sizeof(char));
 	if (!ret)
 		return (NULL);
-	len = ft_strlen(cmd_line);
+	len = ft_strlen(str);
+	redi_trigger = FALSE;
 	solo_quo_trigger = FALSE;
 	duo_quo_trigger = FALSE;
 	dollar_trigger = FALSE;
 	i = -1;
 	j = 0;
 	k = 0;
-	while (++i < len && cmd_line[i])
+	while (++i < len && str[i])
 	{
-		if (cmd_line[i] == '\'' && !solo_quo_trigger && !duo_quo_trigger)
+		if (str[i] == '\'' && !solo_quo_trigger && !duo_quo_trigger)
 		{
-			ret[j++] = cmd_line[i];
+			ret[j++] = str[i];
 			i++;
 			solo_quo_trigger = TRUE;
 		}
-		if (cmd_line[i] == '"' && !duo_quo_trigger && !solo_quo_trigger)
+		if (str[i] == '"' && !duo_quo_trigger && !solo_quo_trigger)
 		{
-			ret[j++] = cmd_line[i];
+			ret[j++] = str[i];
 			i++;
 			duo_quo_trigger = TRUE;
 		}
-		if (i < len - 2 && cmd_line[i] == '$' && !(cmd_line[i + 1] >= 65 && cmd_line[i + 1] <= 90) && !(cmd_line[i + 1] >= 97 && cmd_line[i + 1] <= 122) && !solo_quo_trigger)
+		if (i < len - 1 && ((str[i] == '<' && str[i + 1] != '<' && (i == 0 || str[i - 1] != '<')) || (str[i] == '>' && str[i + 1] != '>' && (i == 0 || str[i - 1] != '>'))) && !solo_quo_trigger && !duo_quo_trigger)
+			redi_trigger = TRUE;
+		else if (i < len - 1 && ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>')) && !solo_quo_trigger && !duo_quo_trigger)
+			redi_trigger = TRUE;
+		else if (i < len - 1 && ((str[i] == '<' && str[i + 1] != '<' && (i == 0 || str[i - 1] != '<')) || (str[i] == '>' && str[i + 1] != '>' && (i == 0 || str[i - 1] != '>'))) && (solo_quo_trigger || duo_quo_trigger))
+			redi_trigger = FALSE;
+		else if (i < len - 1 && ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>')) && (solo_quo_trigger || duo_quo_trigger))
+			redi_trigger = FALSE;
+		if (i < len - 2 && str[i] == '$' && !(str[i + 1] >= 65 && str[i + 1] <= 90) && !(str[i + 1] >= 97 && str[i + 1] <= 122) && !solo_quo_trigger)
 			i += 2;
-		else if (cmd_line[i] == '$' && ((cmd_line[i + 1] >= 65 && cmd_line[i + 1] <= 90) || (cmd_line[i + 1] >= 97 && cmd_line[i + 1] <= 122)) && !solo_quo_trigger)
+		else if (str[i] == '$' && ((str[i + 1] >= 65 && str[i + 1] <= 90) || (str[i + 1] >= 97 && str[i + 1] <= 122)) && !solo_quo_trigger && !redi_trigger)
 			dollar_trigger = TRUE;
-		if (dollar_trigger && ((cmd_line[i] >= 65 && cmd_line[i] <= 90) || (cmd_line[i] >= 97 && cmd_line[i] <= 122)))
+		if (dollar_trigger && ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122)))
 		{
-				env = ft_calloc(find_env_size(cmd_line, i) + 1, sizeof(char));
+				env = ft_calloc(find_env_size(str, i) + 1, sizeof(char));
 				if (!env)
 					return (0);
 				k = 0;
-				env[k++] = cmd_line[i];
-				while (++i < len && ((cmd_line[i] >= 65 && cmd_line[i] <= 90) || (cmd_line[i] >= 97 && cmd_line[i] <= 122) || (cmd_line[i] >= 48 && cmd_line[i] <= 57)))
-					env[k++] = cmd_line[i];
+				env[k++] = str[i];
+				while (++i < len && ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) || (str[i] >= 48 && str[i] <= 57)))
+					env[k++] = str[i];
 				env[k] = '\0';
 				dollar_trigger = FALSE;
 				if (ft_getenv(env, env_lst))
@@ -173,22 +193,22 @@ char *expand_dollar_string(char *cmd_line, t_env *env_lst)
 					env = NULL;
 				}
 		}
-		if (cmd_line[i] == '\'' && solo_quo_trigger && !duo_quo_trigger)
+		if (str[i] == '\'' && solo_quo_trigger && !duo_quo_trigger)
 		{
-			ret[j++] = cmd_line[i];
+			ret[j++] = str[i];
 			i++;
 			solo_quo_trigger = FALSE;
 		}
-		else if (cmd_line[i] == '"' && duo_quo_trigger && !solo_quo_trigger)
+		else if (str[i] == '"' && duo_quo_trigger && !solo_quo_trigger)
 		{
-			ret[j++] = cmd_line[i];
+			ret[j++] = str[i];
 			i++;
 			duo_quo_trigger = FALSE;
 		}
-		if (cmd_line[i] == '$' && ((cmd_line[i + 1] >= 65 && cmd_line[i + 1] <= 90) || (cmd_line[i + 1] >= 97 && cmd_line[i + 1] <= 122)) && !dollar_trigger && !solo_quo_trigger)
+		if (str[i] == '$' && ((str[i + 1] >= 65 && str[i + 1] <= 90) || (str[i + 1] >= 97 && str[i + 1] <= 122)) && !dollar_trigger && !solo_quo_trigger && !redi_trigger)
 			dollar_trigger = TRUE;
-		else if (cmd_line[i] && (!dollar_trigger || cmd_line[i] != '$'))
-			ret[j++] = cmd_line[i];
+		else if (str[i] && (!dollar_trigger || str[i] != '$'))
+			ret[j++] = str[i];
 	}
 	ret[j] = '\0';
 	return (ret);
