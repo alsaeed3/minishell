2e76c74e91295a72a6762dc2e726ee794f25cabd
alsaeed3 <ali.eissa.570@gmail.com>
2024-09-29 10:41:17 +0400

    sovle conflicts

--- libft/src/ft_free_array.c
+++ libft/src/ft_free_array.c
@@ -6,11 +6,7 @@
 /*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 14:33:05 by alsaeed           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/02/13 19:44:42 by alsaeed          ###   ########.fr       */
-=======
-/*   Updated: 2024/02/15 16:23:01 by alsaeed          ###   ########.fr       */
->>>>>>> main
+/*   Updated: 2024/09/29 10:39:43 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +21,10 @@ void	ft_free_array(char ***array)
 	i = 0;
 	while ((*array)[i])
 	{
-<<<<<<< HEAD
-		free_set_null(array[i]);
-		i++;
-	}
-	free_set_null(array);
-=======
 		free_set_null((void **)&(*array)[i]);
 		i++;
 	}
 	free_set_null((void **)array);
->>>>>>> main
 }
 
 void	free_set_null(void **ptr)
